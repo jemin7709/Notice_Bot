82174443e0ba55dd1141cbcd2a80739d06aab25e
Jaemin Park <68144124+jemin7709@users.noreply.github.com>
2021-06-21 15:45:51 +0900

    Update ntBot.py

--- ntBot.py
+++ ntBot.py
@@ -16,8 +16,7 @@ def checkNotice(link, day, Type):
         s.max_redirects = 100
         response = s.get(link)
         html = response.text
-        s.connection.close()
-
+        response.connection.close()
 
     if day.month < 10:
         if day.day < 10:
@@ -34,18 +33,9 @@ def checkNotice(link, day, Type):
             today = str(day.year) + "." + str(day.month) + \
                 "." + str(day.day) + "."
                 
-    if Type != "Inha":
-        today = today[:-1]
-
     bs = bs4.BeautifulSoup(html, 'html.parser')
     tr = bs.find_all("tr")
-
-    if Type == "Inha":
-        message = "`" + today + " 전체 공지사항`"
-    elif Type == "Cse":
-        message = "`" + today + " 컴퓨터공학과 공지사항`"
-    elif Type == "Ee":
-        message = "`" + today + " 전자공학과 공지사항`"
+    message = "`" + today + " 전체 공지사항`"
     post_message(myToken, "#학교-공지", message)
     print(message)
 
@@ -56,42 +46,35 @@ def checkNotice(link, day, Type):
                 if elem2.get_text() == today:
                     tdTitle = elem1.find("td", {"class": "_artclTdTitle"})
                     title = tdTitle.find("a")
-
                     message = title.get_text().strip()
                     message = message.replace("새글", '')
                     post_message(myToken, "#학교-공지", message)
                     print(message)
-
-                    if Type == "Inha":
-                        message = "https://www.inha.ac.kr" + title.get("href")
-                    elif Type == "Cse":
-                        message = "https://cse.inha.ac.kr" + title.get("href")
-                    elif Type == "Ee":
-                        message = "https://ee.inha.ac.kr" + title.get("href")
+                    message = "https://www.inha.ac.kr" + title.get("href")
                     post_message(myToken, "#학교-공지", message)
                     print(message)
 
-                
+
+isNotice = True                
 while True:
     try:
         myToken = ""
         linkInha = "https://www.inha.ac.kr/kr/950/subview.do"
-        linkCse = "https://cse.inha.ac.kr/cse/888/subview.do"
-        linkEe = "https://ee.inha.ac.kr/ee/784/subview.do"
     
         day = datetime.datetime.now()
         start_time = day.replace(hour=19, minute=0, second=0, microsecond=0)
         end_time = day.replace(hour=20, minute=0, second=0, microsecond=0)
-        if start_time < day < end_time:
-            checkNotice(linkInha, day, "Inha")
-            time.sleep(3)
-            checkNotice(linkCse, day, "Cse")
-            time.sleep(3)
-            checkNotice(linkEe, day, "Ee")
-            time.sleep(3)
 
-        time.sleep(3600)
+        if start_time < day < end_time:
+            if isNotice:
+                checkNotice(linkInha, day, "Inha")
+                isNotice = False
+                print('7')
+        else:
+            print('not7')
+            isNotice = True
+        time.sleep(50)
     except Exception as e:
         print(e)
         post_message(myToken, "#공지봇-에러로그", e)
-        time.sleep(3600)
+        isNotice = True
